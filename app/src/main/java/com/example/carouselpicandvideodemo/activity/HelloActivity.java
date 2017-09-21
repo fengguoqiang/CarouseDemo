51b36c3d8ce76177ab9615e8fd3bc4e16e59534e
尼泊尔 <1375343423@163.com>
2017-09-21 18:21:36 +0800

    tj

---
+++ app/src/main/java/com/example/carouselpicandvideodemo/activity/HelloActivity.java
@@ -0,0 +1,10 @@
+package com.example.carouselpicandvideodemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/9/21.
+ */
+
+public class HelloActivity extends AppCompatActivity {
+}
